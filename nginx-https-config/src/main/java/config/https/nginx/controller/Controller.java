d9ca533eb0479e1c20c2ec94d8af11e549f83c09
孟祥昆 <854956123@qq.com>
2019-08-20 21:29:23 +0800

    update

--- nginx-https-config/src/main/java/config/https/nginx/controller/Controller.java
+++ nginx-https-config/src/main/java/config/https/nginx/controller/Controller.java
@@ -1,6 +1,8 @@
 package config.https.nginx.controller;
 
 import config.https.nginx.constants.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Controller {
+	private final Logger logger= LoggerFactory.getLogger(getClass());
 	@GetMapping("/")
 	public String getOk() {
+	logger.info(Constant.OK);
 		return Constant.OK;
 	}
 }
